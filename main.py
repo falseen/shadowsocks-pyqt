b6f235fc5f957cb8d28a2db049da95640558d9b9
Falseen <hurxyill@163.com>
2017-03-10 04:16:18 +0800

    update gui-config.json main.py
    mkdir lib files

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ import sys
 import os
 import json
 import logging
+import time
+
 
 def read_json(title):
     save_path = os.path.join(sys.path[0], title)
@@ -24,7 +26,7 @@ def read_json(title):
         j = {}
     return j    
 
-def savejson(title, json_dict):
+def save_json(title, json_dict):
     save_path = os.path.join(sys.path[0], title)
     with open(save_path , mode = 'w') as f : 
         json.dump(json_dict,f, ensure_ascii = False ,indent=2)   
@@ -43,7 +45,30 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         """
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
-    
+
+    def save_config(self):
+        new_config = {}
+        gui_config = self.gui_config
+        new_config["server"] = self.serverAddrEdit.text()
+        new_config["server_port"] = self.serverPortSpinBox.value()
+        new_config["password"] = self.pwdEdit.text()
+        new_config["method"] = self.encryptComboBox.currentText()
+        new_config["timeout"] = self.timeoutSpinBox.value()
+        new_config["one_time_auth"] = self.otaCheckBox.isChecked()
+        save_json("config.json", new_config)
+        new_config["remarks"] = self.remarksEdit.text()
+        index = self.configlist.currentRow()
+        if index < len(gui_config["configs"]):
+            gui_config["configs"][index] = new_config
+        else:    
+            gui_config["configs"].append(new_config)
+        save_json("gui-config.json", gui_config)
+        if new_config.get("remarks","") == "":
+            item_text = "%s:%s" %(new_config["server"], new_config["server_port"])
+        else:
+            item_text = "%s (%s:%s)" %(new_config["remarks"], new_config["server"], new_config["server_port"])      
+        self.configlist.currentItem().setText(item_text)
+
     @pyqtSlot()
     def on_b_yes_clicked(self):
         """
@@ -52,6 +77,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         logging.info("close ss-local...")
         self.sslocal_process.terminate()
         self.sslocal_process.join()
+        self.save_config()
         logging.info("restart ss-local")
         sslocal_process = Process(target=local.main, daemon = True)
         sslocal_process.start()
@@ -60,19 +86,52 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     @pyqtSlot()
     def on_b_exit_clicked(self):
-        """
-        Slot documentation goes here.
-        """
-        # TODO: not implemented yet
+        self.sslocal_process.terminate()
+        self.sslocal_process.join()
+
+    @pyqtSlot()
+    def on_del_config_clicked(self):
+        index = self.configlist.currentRow()
+        self.configlist.takeItem(index)
+        if index < len(self.gui_config["configs"]):
+            self.gui_config["configs"].pop(index)
+        self.update()    
 
     @pyqtSlot()
     def on_add_config_clicked(self):
-        """
-        Slot documentation goes here.
-        """
-        # TODO: not implemented yet
-        
-        raise NotImplementedError
+        new_config = {}
+        count = self.configlist.count()
+        self.configlist.addItem("127.0.0.1")
+        new_config["server"] = "127.0.0.1"
+        new_config["server_port"] = 1080
+        new_config["password"] = "password"
+        new_config["method"] = "aes-256-cfb"
+        new_config["timeout"] = 600
+        new_config["one_time_auth"] = False
+        new_config["remarks"] = ""
+        self.gui_config["configs"].append(new_config)
+        self.configlist.setCurrentRow(count)
+        self.update()
+
+    def update(self):
+        user_json = self.gui_config
+        index = self.configlist.currentRow()
+        select_config = user_json["configs"][index]
+        remarks = select_config.get("remarks","")
+        server_addr = select_config["server"]
+        server_port = select_config["server_port"]
+        password = select_config["password"]
+        method = select_config["method"]
+        timeout = select_config["timeout"]
+        one_time_auth = select_config["one_time_auth"]
+        self.serverAddrEdit.setText(server_addr)
+        self.serverPortSpinBox.setValue(int(server_port))
+        self.pwdEdit.setText(password)
+        self.encryptComboBox.setCurrentText(method)
+        self.timeoutSpinBox.setValue(timeout)
+        self.otaCheckBox.setChecked(one_time_auth)
+        self.remarksEdit.setText(remarks)
+
 
 
 if __name__ == "__main__":
@@ -85,10 +144,17 @@ if __name__ == "__main__":
     sslocal_process = Process(target=local.main, daemon = True)
     sslocal_process.start()
     mySW.sslocal_process = sslocal_process
-    user_json = read_json("user.json")
+    mySW.gui_config = read_json("gui-config.json")
     configlist = mySW.configlist
-    for k,v in user_json.items():
-        configlist.addItem(k)
+    for x in mySW.gui_config.get("configs",{}):
+        if x.get("remarks","") == "":
+            item_text = "%s:%s" %(x["server"], x["server_port"])
+        else:
+            item_text = "%s (%s:%s)" %(x["remarks"],x["server"], x["server_port"])    
+        configlist.addItem(item_text)
+    index = mySW.gui_config.get("index", 0)
+    configlist.setCurrentRow(index)
+    mySW.update()
     mySW.show()
     sys.exit(app.exec_())
     
