d47cc7cfa1bdadec7fbec94ac381dff693603661
Falseen <hurxyill@163.com>
2017-03-11 23:52:59 +0800

    update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-rd /S /Q dist
+-rd /S /Q dist
 git submodule init
 git submodule update
 move /Y .\shadowsocks\shadowsocks\* shadowsocks\
@@ -7,7 +7,7 @@ pyinstaller -y main_onefile.spec
 cd dist
 mkdir shadowsocks_tmp
 move shadowsocks-pyqt* shadowsocks_tmp\
-xcopy /Y /I ..\res shadowsocks_tmp\
+xcopy /Y /I ..\res shadowsocks_tmp\res
 copy /Y ..\config.json shadowsocks_tmp\
 copy /Y ..\gui-config.json shadowsocks_tmp\
 move shadowsocks_tmp shadowsocks-pyqt
